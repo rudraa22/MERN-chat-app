58a2186c642ebf5d6b7729e3f53dad045d61c831
Kanodiya Rudra <rudrak2225@gmail.com>
2026-08-03 20:00:16 +0530

    Add message model, controller and routes for sending/fetching messages

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,40 @@ const io = new Server(server, {
 io.on('connection' , (socket) => {
     console.log('New client connected' , socket.id);
 
+    socket.on('setup' , (userId) => {
+        socket.join(userId);
+        socket.emit('connected');
+        });
+
+    socket.on(('join chat' , (chatId) => {
+        socket.join(chatId);
+        console.log('User joined chat' , chatId);
+    }));
+    
+    socket.on('typing' , (chatId) => {
+        socket.in(chatId).emit('typing');
+    });
+
+    socket.on('sstop typing' , (chatId) => {
+        socket.in(chatId).emit('stop typing');
+    });
+
+    socket.on('new message' , (newMessageReceived) => {
+        const chat = newMessageReceived.chat;
+
+        if(!chat.users)
+        {
+            console.log('chat.users not defined');
+            return;
+        }
+
+        chat.users.forEach((user) => {
+            if(user._id === newMessageReceived.sender._id)
+                return;
+            socket.in(user._id).emit('message recieved' , newMessageReceived);
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected' , socket.id);
     });
